5640d40400d8c4a80d2c39e0219a721c734ba734
bosco250 <dusengimana06@gmail.com>
2024-11-22 10:09:18 +0200

    time

--- src/Components/ReserveHereNow.tsx
+++ src/Components/ReserveHereNow.tsx
@@ -15,21 +15,40 @@ function ReserveHereNow() {
       { time: "12:58 AM" }
     ]
   };
-  
+
+  const getMealType = () => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+
+    if (hours >= 6 && hours < 11) {
+      return "Breakfast";  
+    } else if (hours >= 11 && hours < 17) {
+      return "Lunch";  
+    } else {
+      return "Dinner";  
+    }
+  };
+
+  const mealType = getMealType(); 
+
   return (
     <div className="px-14 pt-10 w-screen md:mb-8">
       <div className="flex justify-between mb-8">
-        <h1 className="text-3xl font-bold text-gray-800">Book Lunch in Kigali now</h1>
-        <span className="text-sm text-[#DF7117] flex items-center">View all   <ChevronRight /></span>
+        <h1 className="text-3xl font-bold text-gray-800">
+          Book {mealType} in Kigali now
+        </h1>
+        <span className="text-sm text-[#DF7117] flex items-center">
+          View all <ChevronRight />
+        </span>
       </div>
       <div className="flex gap-5">
-      <RestaurantCard {...restaurantData} /> 
-      <RestaurantCard {...restaurantData} /> 
-      <RestaurantCard {...restaurantData} /> 
-      <RestaurantCard {...restaurantData} /> 
+        <RestaurantCard {...restaurantData} /> 
+        <RestaurantCard {...restaurantData} /> 
+        <RestaurantCard {...restaurantData} /> 
+        <RestaurantCard {...restaurantData} /> 
       </div>     
     </div>
   )
 }
 
-export default ReserveHereNow
+export default ReserveHereNow;
